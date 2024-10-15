bc0fbf266e66cf0f090209c5b53d0b2c097df9f8
Luke Maurer <lmaurer@janestreet.com>
2024-10-15 18:22:38 +0100

    Raw files for pivot_root

---
+++ tools/unlabel-patches/generate_cached_generic_functions.ml
@@ -0,0 +1,88 @@
+(**********************************************************************************
+ *                             MIT License                                        *
+ *                                                                                *
+ *                                                                                *
+ * Copyright (c) 2023 Jane Street Group LLC                                       *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ *                                                                                *
+ **********************************************************************************)
+
+open Printf
+open Misc
+open Config
+
+module CU = Compilation_unit
+
+let make_cached_generic_functions unix ~ppf_dump ~id genfns =
+  Location.input_name := Generic_fns.Partition.name id; (* set name of "current" input *)
+  let startup_comp_unit = Generic_fns.Partition.to_cu id in
+  Compilenv.reset startup_comp_unit;
+  Emit.begin_assembly unix;
+  let compile_phrase p = Asmgen.compile_phrase ~ppf_dump p in
+  Profile.record_call "genfns" (fun () ->
+    List.iter compile_phrase (Generic_fns.compile ~shared:true genfns));
+ Emit.end_assembly ()
+
+let cached_generic_functions unix ~ppf_dump ~id output_name genfns =
+  Profile.record_call output_name (fun () ->
+    let startup = output_name ^ ext_asm in
+    Profile.record_call "compile_unit" (fun () ->
+      let obj_filename = output_name ^ ext_obj in
+      Asmgen.compile_unit ~output_prefix:output_name
+        ~asm_filename:startup ~keep_asm:false
+        ~obj_filename
+        ~may_reduce_heap:true
+        ~ppf_dump
+        (fun () ->
+          make_cached_generic_functions unix ~ppf_dump ~id genfns);
+      obj_filename
+    );
+  )
+
+let main filename =
+  let unix = (module Unix : Compiler_owee.Unix_intf.S) in
+  Clflags.native_code := true;
+  Clflags.use_linscan := true;
+  Compmisc.init_path ();
+  let file_prefix = Filename.remove_extension filename ^ ext_lib in
+  let genfns_partitions = Generic_fns.Cache.all () in
+  let objects = ref [] in
+  Fun.protect
+    ~finally:(fun () -> List.iter remove_file !objects)
+    (fun () ->
+       Hashtbl.iter (fun name partition ->
+         let output_name = Filename.temp_file ("cached-generated-" ^ Generic_fns.Partition.to_string name) "" in
+         let obj =
+           cached_generic_functions
+             unix ~ppf_dump:Format.std_formatter ~id:name output_name partition
+         in
+         objects := obj :: !objects
+       ) genfns_partitions;
+       ignore (Ccomp.create_archive file_prefix !objects : int))
+
+let arg_usage =
+  Printf.sprintf "%s FILE : Generate an obj file containing cached generatic functions named FILE" Sys.argv.(0)
+
+let main () =
+  Memtrace.trace_if_requested ~context:"ocamlopt" ();
+  Arg.parse_expand [] main arg_usage;
+  exit 0
+
+let _ = main ()
---
+++ tools/unlabel-patches/merge_archives.ml
@@ -0,0 +1,177 @@
+(**************************************************************************
+ *                                                                        *
+ *                                 OCaml                                  *
+ *                                                                        *
+ *                    Mark Shinwell, Jane Street Europe                   *
+ *                                                                        *
+ *   Copyright 2021 Jane Street Group LLC                                 *
+ *                                                                        *
+ *   All rights reserved.  This file is distributed under the terms of    *
+ *   the GNU Lesser General Public License version 2.1, with the          *
+ *   special exception on linking described in the file LICENSE.          *
+ *                                                                        *
+ **************************************************************************)
+
+module String_set = Set.Make (String)
+
+let merge_cma ~target ~archives =
+  (* This is rather tightly tied to the internals of [Bytelibrarian]. *)
+  Clflags.link_everything := false;
+  Clflags.custom_runtime := false;
+  Clflags.no_auto_link := false;
+  Clflags.ccobjs := [];
+  Clflags.all_ccopts := [];
+  Clflags.dllibs := [];
+  List.iter
+    (fun archive -> Load_path.add_dir ~hidden:false (Filename.dirname archive))
+    archives;
+  let error reporter err =
+    Format.eprintf "Error whilst merging .cma files:@ %a\n%!" reporter err;
+    exit 1
+  in
+  try
+    Bytelibrarian.create_archive archives target;
+    Warnings.check_fatal ()
+  with
+  | Bytelibrarian.Error err -> error Bytelibrarian.report_error err
+  | Bytelink.Error err -> error Bytelink.report_error err
+  | Warnings.Errors ->
+    (* Warnings should already have been printed to stderr. *)
+    exit 1
+
+let read_cmxa filename =
+  let chan = open_in_bin filename in
+  let magic =
+    really_input_string chan (String.length Config.cmxa_magic_number)
+  in
+  let (cmxa : Cmx_format.library_infos) = input_value chan in
+  close_in chan;
+  magic, cmxa
+
+let merge_cmxa0 ~archives =
+  let magic_and_cmxa_list = List.map read_cmxa archives in
+  let magics = List.map fst magic_and_cmxa_list in
+  let cmxa_list = List.map snd magic_and_cmxa_list in
+  let magic =
+    match String_set.elements (String_set.of_list magics) with
+    | [magic] -> magic
+    | _ :: _ -> failwith "Archives do not agree on the .cmxa magic number"
+    | [] -> assert false
+  in
+  let ncmxs = ref 0 and ncmis = ref 0 in
+  let cmi_table = Hashtbl.create 42 in
+  let cmx_table = Hashtbl.create 42 in
+  cmxa_list
+  |> List.iter (fun (lib : Cmx_format.library_infos) ->
+         lib.lib_imports_cmi
+         |> Array.iter (fun import ->
+                let name = Import_info.name import in
+                if not (Hashtbl.mem cmi_table name)
+                then begin
+                  Hashtbl.add cmi_table name (import, !ncmis);
+                  incr ncmis
+                end);
+         lib.lib_imports_cmx
+         |> Array.iter (fun import ->
+                let cu = Import_info.cu import in
+                if not (Hashtbl.mem cmx_table cu)
+                then begin
+                  Hashtbl.add cmx_table cu (import, !ncmxs);
+                  incr ncmxs
+                end));
+  let cmis = Array.make !ncmis Import_info.dummy in
+  Hashtbl.iter (fun _name (import, i) -> cmis.(i) <- import) cmi_table;
+  let cmxs = Array.make !ncmxs Import_info.dummy in
+  Hashtbl.iter (fun _name (import, i) -> cmxs.(i) <- import) cmx_table;
+  let genfns = Generic_fns.Tbl.make () in
+  let _, lib_units, lib_ccobjs, lib_ccopts =
+    List.fold_left
+      (fun (lib_names, lib_units, lib_ccobjs, lib_ccopts)
+           (cmxa : Cmx_format.library_infos) ->
+        let new_lib_names =
+          List.map
+            (fun (cmx : Cmx_format.lib_unit_info) -> cmx.li_name)
+            cmxa.lib_units
+          |> Compilation_unit.Set.of_list
+        in
+        let already_defined =
+          Compilation_unit.Set.inter new_lib_names lib_names
+        in
+        if not (Compilation_unit.Set.is_empty already_defined)
+        then failwith "Archives contain multiply-defined units";
+        ignore(Generic_fns.Tbl.add
+                ~imports:Generic_fns.Partition.Set.empty
+                genfns cmxa.lib_generic_fns);
+        let lib_names = Compilation_unit.Set.union new_lib_names lib_names in
+        let remap oldarr newarr tbl oldb ~get_key =
+          let module B = Misc.Bitmap in
+          let b = B.make (Array.length newarr) in
+          oldb
+          |> B.iter (fun i ->
+                 B.set b (snd (Hashtbl.find tbl (get_key oldarr.(i)))));
+          b
+        in
+        let new_units =
+          List.map
+            (fun (li : Cmx_format.lib_unit_info) ->
+              { li with
+                li_imports_cmi =
+                  remap cmxa.lib_imports_cmi cmis cmi_table li.li_imports_cmi
+                    ~get_key:Import_info.name;
+                li_imports_cmx =
+                  remap cmxa.lib_imports_cmx cmxs cmx_table li.li_imports_cmx
+                    ~get_key:Import_info.cu
+              })
+            cmxa.lib_units
+        in
+        let lib_units = lib_units @ new_units in
+        let cmxa_lib_ccobjs = String_set.of_list cmxa.lib_ccobjs in
+        let lib_ccobjs = String_set.union cmxa_lib_ccobjs lib_ccobjs in
+        let lib_ccopts = lib_ccopts @ cmxa.lib_ccopts in
+        lib_names, lib_units, lib_ccobjs, lib_ccopts)
+      (Compilation_unit.Set.empty, [], String_set.empty, [])
+      cmxa_list
+  in
+  let cmxa : Cmx_format.library_infos =
+    { lib_units;
+      lib_ccobjs = String_set.elements lib_ccobjs;
+      lib_ccopts;
+      lib_imports_cmi = cmis;
+      lib_imports_cmx = cmxs;
+      lib_generic_fns = Generic_fns.Tbl.entries genfns
+    }
+  in
+  magic, cmxa
+
+let merge_cmxa ~target ~archives =
+  let magic, cmxa = merge_cmxa0 ~archives in
+  let chan = open_out_bin target in
+  output_string chan magic;
+  output_value chan cmxa;
+  close_out chan
+
+let has_extension archive ~ext = Filename.check_suffix archive ("." ^ ext)
+
+let syntax () =
+  Printf.eprintf
+    "syntax: %s OCAMLOPT-BINARY TARGET-CMA-OR-CMXA-FILE CMA-OR-CMXA-FILES\n"
+    Sys.argv.(0);
+  Printf.eprintf "Please provide only .cma files or only .cmxa files.";
+  exit 1
+
+let () =
+  if Array.length Sys.argv < 3 then syntax ();
+  let target = Sys.argv.(1) in
+  let archives =
+    Array.sub Sys.argv 2 (Array.length Sys.argv - 2) |> Array.to_list
+  in
+  let all_cma =
+    List.for_all (fun archive -> has_extension archive ~ext:"cma") archives
+  in
+  let all_cmxa =
+    List.for_all (fun archive -> has_extension archive ~ext:"cmxa") archives
+  in
+  match all_cma, all_cmxa with
+  | true, false -> merge_cma ~target ~archives
+  | false, true -> merge_cmxa ~target ~archives
+  | true, true | false, false -> syntax ()
