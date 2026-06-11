16cddc6c7a303a85238acb42cf3ffe7a0d7de9a5
Ryan Tjoa <rtjoa@janestreet.com>
2026-06-11 11:00:27 -0400

    Cherry-pick current state of #6307

--- typing/typecore.ml
+++ typing/typecore.ml
@@ -1905,7 +1905,8 @@ and build_as_type_aux (env : Env.t) p ~mode =
 let is_variable_repres : type rep. rep record_form -> rep -> bool =
   fun form rep ->
     match form, rep with
-    | Legacy, Record_variable -> true
+    | Legacy, (Record_variable | Record_inlined (_, Constructor_variable, _)) ->
+      true
     | Unboxed_product, Record_unboxed_product_variable -> true
     | _ -> false
 
@@ -1940,6 +1941,7 @@ let update_labels (type rep) env (form : rep record_form) ~representative_label
       in
       match
         Typedecl.update_record_representation ~why env loc form
+          ~old_repres:representative_label.lbl_repres
           (lbls_and_ty_args |> Array.to_list)
       with
       | Ok (sorts, rep) ->
@@ -3079,7 +3081,7 @@ let keep_scopes tys ~f =
 let representation_for_tuple_constructor env constr ty_args ~loc ~types
       ~containing_type ~why : _ Result.t =
   match constr.cstr_shape with
-  | Some shape ->
+  | (Constructor_uniform_value | Constructor_mixed _) as shape ->
       begin match
         Misc.Stdlib.List.map_option
           (fun arg -> arg.ca_sort |> Option.map Jkind.Sort.of_const)
@@ -3088,7 +3090,7 @@ let representation_for_tuple_constructor env constr ty_args ~loc ~types
       | Some sorts -> Ok (shape, sorts)
       | None -> Misc.fatal_error "representable constructor missing a sort"
       end
-  | None ->
+  | Constructor_variable ->
       (* XXX Claude suggested keeping the scopes as a fix, and it works, but its
          explanation for the correctness was not good. Need to be convinced that
          it's safe to poke these holes in the scopes. *)
@@ -6822,7 +6824,8 @@ and type_expect_
                  each label all over again. Possibly we're doing things in the
                  wrong order. *)
               Typedecl.update_record_representation ~why env
-                sexp.pexp_loc record_form labels_with_updated_types
+                sexp.pexp_loc record_form ~old_repres:representation
+                labels_with_updated_types
             with
             | Ok (_, rep) -> rep
             | Error _ ->
